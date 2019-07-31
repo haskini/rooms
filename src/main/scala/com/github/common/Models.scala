410cc85be4dd2aa4e3abc66686a81fb7cc51fb0f
Nadezda Agildina <a_strelina@inbox.ru>
2019-07-31 20:30:42 +0300

    [ADD] Rooms models

--- src/main/scala/com/github/common/Models.scala
+++ src/main/scala/com/github/common/Models.scala
@@ -1,19 +1,36 @@
 package com.github.common
 
+import akka.http.scaladsl.model.DateTime
+
+// COMMON MODELS
+final case class User(email: String, password: String, name: String, isAdmin: Boolean)
+
+final case class Booking(time: DateTime, owner: String)
+final case class Room(number: String, bookings: Seq[Booking])
+final case class Rooms(rooms: Seq[Room])
+
 // IN MODELS
 
-final case class dbUser(email: String, name: String, isAdmin: String, password: String)
 final case class GetUserIn(email: String)
 final case class CheckPasswordIn(email: String, password: String)
-final case class CreateUserIn(email: String, name: String, isAdmin: String, password: String)
-final case class UpdateUserIn(oldEmail: String, email: String, name: String, isAdmin: String)
+final case class CreateUserIn(user: User)
+final case class UpdateUserIn(oldEmail: String, email: String, name: String, isAdmin: Boolean)
 final case class UpdatePasswordIn(email: String, oldPassword: String, newPassword: String)
 final case class DeleteUserIn(email: String, password: String)
 
+final case class GetRoomIn(number: Int)
+final case class CreateRoomIn(room: Room)
+final case class BookRoomIn(number: String, booking: Booking)
+final case class FreeRoomIn(number: String, time: DateTime)
+final case class DeleteRoomIn(number: String)
+
 // OUT MODELS
 
-final case class GetUserOut(email: String, password: String, name: String, isAdmin: String)
-final case class GetUsersOut(users: List[GetUserOut])
+final case class GetUserOut(user: User)
+final case class GetUsersOut(users: List[User])
+
+final case class RoomOut(room: Room)
+final case class GetRoomsOut(rooms: List[Room])
 
 // ERRORS
 
