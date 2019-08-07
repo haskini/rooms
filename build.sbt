43ad93b3ef1e774ddabc15abef51ccf3b934e879
Nadezda Agildina <a_strelina@inbox.ru>
2019-08-07 15:55:20 +0300

    [ADD] DbUserTest, [UPD] build.sbt, comment all UserRoutesSpec

--- build.sbt
+++ build.sbt
@@ -3,6 +3,9 @@ lazy val akkaVersion = "2.6.0-M5"
 lazy val mongoDriverVersion = "2.6.0"
 lazy val json4sVersion = "3.6.7"
 lazy val jwtVersion = "3.1.0"
+lazy val scalatestVersion = "3.0.5"
+lazy val softwaremillVersion = "1.6.3"
+lazy val EmbedmongoVersion = "0.2.4"
 
 lazy val root = (project in file(".")).
     settings(
@@ -21,10 +24,14 @@ lazy val root = (project in file(".")).
             
             "org.json4s" %% "json4s-native" % json4sVersion,
             "com.pauldijou" %% "jwt-json4s-native" % jwtVersion,
-            
+    
+            "org.slf4j" % "slf4j-simple" % "1.6.2" % Test,
             "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
             "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
             "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test,
-            "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-        )
-    )
+            "org.scalatest" %% "scalatest" % scalatestVersion % Test,
+            "com.softwaremill.sttp" %% "core" % softwaremillVersion % Test,
+            "com.github.simplyscala" %% "scalatest-embedmongo" % EmbedmongoVersion % Test
+        
+        parallelExecution in Test := false
+)
---
+++ src/test/scala/com/github/persistence/DbUserTest.scala
@@ -0,0 +1,147 @@
+package com.github.persistence
+
+import com.github.common.DbModels
+import com.github.persistence.DbUser._
+import org.scalatest.{Assertion, AsyncFunSuite, Matchers}
+import scala.concurrent.Future
+
+class DbUserTest extends AsyncFunSuite with Matchers {
+
+    val userInDb1 = DbModels.User("test1@email.com", "12345", "TestName1", isAdmin = true)
+    val userInDb2 = DbModels.User("test2@email.com", "54321", "TestName2", isAdmin = false)
+    
+    test("Get User1 from MongoDB") {
+        GetUser("test1@email.com") map {
+            case Right(r) => r shouldBe userInDb1
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Get Users(0,0) size from MongoDB should be 2" ) {
+        GetUsers(0, 0) map {
+            case Right(r) => r.size shouldBe 2
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Get Users(0,0) from MongoDB should be List(<User1>, <User2>)" ) {
+        GetUsers(0, 0) map {
+            case Right(r) => r shouldBe List(
+                userInDb1,
+                userInDb2
+            )
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Get Users(1,0) from MongoDB should be List(<User2>)" ) {
+        GetUsers(1, 0) map {
+            case Right(r) => r shouldBe List(
+                userInDb2
+            )
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Get Users(0,1) from MongoDB should be List(<User1>)" ) {
+        GetUsers(0, 1) map {
+            case Right(r) => r shouldBe List( userInDb1 )
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Get Users(1,1) from MongoDB should be List(<User2>)" ) {
+        GetUsers(1, 1) map {
+            case Right(r) => r shouldBe List( userInDb2 )
+            case Left(_) => fail
+        }
+    }
+    
+    
+    def checkUser(email: String, checkedUser: DbModels.User): Future[Assertion] = {
+        GetUser(email) map {
+            case Right(user) => user shouldBe checkedUser
+            case Left(_) => fail
+        }
+    }
+    
+    test ("Create User in MongoDB") {
+        val createdUser = DbModels.User("createUserEmail@email.com", "09876", "CreatedUser", isAdmin = false)
+    
+        CreateUser(createdUser) map {
+            case Some(_) => fail // equal to: case error => fail
+            case None => checkUser("createUserEmail@email.com", createdUser)
+        }
+    
+        DeleteUser("createUserEmail@email.com") map {
+            case Some(_) => fail
+            case None => succeed
+        }
+    }
+    
+    def updateUserInDb2ToOriginal(): Future[Assertion] ={
+        UpdateUser("test2@email.com", userInDb2) map {
+            case Some(_) => fail
+            case None => succeed
+        }
+    }
+    
+    test("Update User's isAdmin field in MongoDB") {
+        val updatedUser: DbModels.User = userInDb2.copy(isAdmin = true)
+        
+        UpdateUser("test2@email.com", updatedUser) map {
+            case Some(_) => fail
+            case None => checkUser("test2@email.com", updatedUser)
+        }
+        updateUserInDb2ToOriginal()
+    }
+    
+    test("Update User's name field in MongoDB") {
+        val updatedUser: DbModels.User = userInDb2.copy(name = "UpdatedUserName")
+        
+        UpdateUser("test2@email.com", updatedUser) map {
+            case Some(_) => fail
+            case None => checkUser("test2@email.com", updatedUser)
+        }
+    
+        updateUserInDb2ToOriginal()
+    }
+    
+    test("Update User's passHash field in MongoDB") {
+        val updatedUser: DbModels.User = userInDb2.copy(passHash = "UpdatedPassHash")
+        
+        UpdateUser("test2@email.com", updatedUser) map {
+            case Some(_) => fail
+            case None => checkUser("test2@email.com", updatedUser)
+        }
+        
+        updateUserInDb2ToOriginal()
+    }
+    
+    test("Update User's email field in MongoDB") {
+        val updatedUser: DbModels.User = userInDb2.copy(email = "Updated@email.com")
+        
+        UpdateUser("test2@email.com", updatedUser) map {
+            case Some(_) => fail
+            case None => checkUser("test2@email.com", updatedUser)
+        }
+    
+        // and than return original userInDb2
+        UpdateUser("Updated@email.com", userInDb2) map {
+            case Some(_) => fail
+            case None => succeed
+        }
+    }
+    
+    test ("Delete User from MongoDB") {
+        DeleteUser("test2@email.com") map {
+            case Some(_) => fail
+            case None => succeed
+        }
+        
+        CreateUser(userInDb2) map {
+            case Some(_) => fail
+            case None => succeed
+        }
+    }
+}
